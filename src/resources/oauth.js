15fef9070a23daffe9c580b209303066b9736056
Aliaksandr_Khatsko <Aliaksandr_Khatsko@epam.com>
2018-02-19 15:02:02 +0300

    Update oauth.js with createAccountsToken method

--- src/resources/oauth.js
+++ src/resources/oauth.js
@@ -1,13 +1,25 @@
-import { API_URL, WEB_BASE_URL, postRequest } from '../shared';
+import { API_URL, WEB_BASE_URL, ACCOUNTS_BASE_URL, postRequest } from '../shared';
 
-const basicToken = `Basic ${new Buffer(`${WEB_BASE_URL}:${WEB_BASE_URL}`).toString('base64')}`;
-const AUTH = {
-  Authorization: basicToken,
-  'Content-Type': 'application/x-www-form-urlencoded',
-};
-const REQUEST = { baseUrl: `${API_URL}`, headers: AUTH, json: false };
+function getRequest(baseURL) {
+  const basicToken = `Basic ${new Buffer(`${baseURL}:${baseURL}`).toString('base64')}`;
+  const AUTH = {
+    Authorization: basicToken,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+  const request = { baseUrl: `${API_URL}`, headers: AUTH, json: false };
+  return request;
+}
 
 export function createToken(user, password) {
+  const REQUEST = getRequest(WEB_BASE_URL);
+  REQUEST.uri = '/oauth/token';
+  return postRequest(
+    { ...REQUEST, body: `username=${user}&password=${password}&grant_type=password` })
+    .then(response => response);
+}
+
+export function createAccountsToken(user, password) {
+  const REQUEST = getRequest(ACCOUNTS_BASE_URL);
   REQUEST.uri = '/oauth/token';
   return postRequest(
     { ...REQUEST, body: `username=${user}&password=${password}&grant_type=password` })
