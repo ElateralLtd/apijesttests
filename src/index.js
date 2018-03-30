593c6cedd2dad0fa210b9289b8a726e48af36fab
Aliaksandr_Khatsko <Aliaksandr_Khatsko@epam.com>
2018-03-30 17:39:19 +0300

    Add console.log statements into index.js for debugging

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ const fs = require('fs');
 const fileName = path.resolve('config.json');
 const config = require(fileName);
 
+console.log('TEST_BEFORE_FOR_DEBUGGING');
+console.log(config.apiFQDN);
+
 if (process.env.apiFQDN) {
   config.apiFQDN = process.env.apiFQDN;
+  console.log('TEST_AFTER_FOR_DEBUGGING');
+  console.log(config.apiFQDN);
   config.accountFQDN = process.env.accountFQDN;
   config.webAppFQDN = process.env.webAppFQDN;
 
